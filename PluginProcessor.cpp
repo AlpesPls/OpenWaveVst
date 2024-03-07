0bd65ffc315f346d58e2d64fdc05e370518a157e
Guilherme Alves <106925527+AlpesPls@users.noreply.github.com>
2024-03-07 14:07:52 +0000

    clean PluginProcessor.cpp

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -26,7 +26,7 @@ TapSynthAudioProcessor::TapSynthAudioProcessor()
 #endif
 {
     synth.addSound (new SynthSound());
-    //synth.addVoice (new SynthVoice());
+    
     
     for (int i = 0; i < 5; i++)
     {
@@ -79,8 +79,7 @@ double TapSynthAudioProcessor::getTailLengthSeconds() const
 
 int TapSynthAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    return 1;   
 }
 
 int TapSynthAudioProcessor::getCurrentProgram()
@@ -119,8 +118,7 @@ void TapSynthAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlo
 
 void TapSynthAudioProcessor::releaseResources()
 {
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
+    
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
@@ -130,13 +128,12 @@ bool TapSynthAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts)
     juce::ignoreUnused (layouts);
     return true;
   #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
+   
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
      && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
-    // This checks if the input layout matches the output layout
+    
    #if ! JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
@@ -210,7 +207,7 @@ void TapSynthAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce
 //==============================================================================
 bool TapSynthAudioProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+    return true; 
 }
 
 juce::AudioProcessorEditor* TapSynthAudioProcessor::createEditor()
@@ -221,9 +218,7 @@ juce::AudioProcessorEditor* TapSynthAudioProcessor::createEditor()
 //==============================================================================
 void TapSynthAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    
 }
 
 void TapSynthAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
@@ -243,7 +238,7 @@ float TapSynthAudioProcessor::getRmsValue(const int channel) const
 }
 
 //==============================================================================
-// This creates new instances of the plugin..
+
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new TapSynthAudioProcessor();
